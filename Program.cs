67a82ab8799571f73fa6d2adc64317cb231423f5
Lion-killer <yura.bilous@gmail.com>
2023-12-02 13:15:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace StringTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var testStr = "A1:B2";
+            var testresult = testStr.Split(':', 2, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
